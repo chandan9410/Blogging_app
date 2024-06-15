4433ce188636fde7095ce9b30b09b20c64461d2d
chandan <chandankoranga728@gmail.com>
2024-06-15 20:06:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  // Dispatch :  accepts an object that represents the type of action
+  // we want to execute when it is called. Basically,
+  //it sends the type of action to the reducer function to perform its job,
+  // which, of course, is updating the state
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
+}
+
+export default App;
---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,42 @@
+// // it is mechanism by  which pages ans routes are protected
+// import React, { useEffect, useState } from "react";
+// import { useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+
+// export default function Protected({ children, authentication = true }) {
+//   const navigate = useNavigate();
+//   const [loader, setLoader] = useState(true);
+//   const authStatus = useSelector((state) => state.auth.status);
+
+//   useEffect(() => {
+
+//     if (authentication && authStatus !== authentication) {
+//       navigate("/login");
+//     } else if (!authentication && authStatus !== authentication) {
+//       navigate("/");
+//     }
+//     setLoader(false);
+//   }, [authStatus, navigate, authentication]);
+
+//   return loader ? <h1>Loading...</h1> : <>{children}</>;
+// }
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth?.status); // Added optional chaining
+
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
+}
